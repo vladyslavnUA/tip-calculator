9f9c9f551df172fb41ba0f0f0820e8355d906b11
vladyslavnUA <vladyslavn@yahoo.com>
2019-11-20 13:09:51 -0800

    added global variable calc

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
 def calculate():
     total = float(input("what is your bill total? (i.e. $22.45) $ "))
     tip = float(input("what is the tip percentage (%)? "))
+    global calc
+    calc = (total * (tip / 100)) + total
+
