4a03f54efb7c6eb6022f51fcb83c5cc61b13f421
vladyslavnUA <vladyslavn@yahoo.com>
2019-11-20 14:21:45 -0800

    more validations, rounding numbers

--- calc.py
+++ calc.py
@@ -2,6 +2,7 @@ import os
 import signal
 
 def calculate():
+    #validate user input for float
     while True:
         try:
             total = float(input("what is your bill total? (i.e. $22.45) $ "))
@@ -11,15 +12,19 @@ def calculate():
             continue
         else:
             break
-        
+    #global calc is available throughout the script
     global calc
+    #entire calculation
     calc = (total * (tip / 100)) + total
+    round(calc, 2)
     print(f'your bill total after a {tip}% tip is ${calc}')
     split()
+    #return calc so it can be user later
     return calc
 
 def split():
     t = calc
+    #validate user input for letter
     while True:
         try:
             ask = input("do you want to split the bill with anyone? type y or n\n > ")
@@ -28,19 +33,31 @@ def split():
             continue
         if ask not in ['y', 'n']:
             print("type in y or n")
+            #will keep asking the user
             continue
         else:
             break
-        
+    #calculating total with # of people
     if ask == 'y':
-        ask2 = int(input("how many people do you want to split the bill with?\n > "))
+        while True:
+            try:
+                ask2 = int(input("how many people do you want to split the bill with?\n > "))
+                break
+            except ValueError:
+                print("please enter an integer. (hint: an integer has no decimals)")
+                continue
+            else:
+                break
         final = (t / ask2)
+        #rounding to two decimals
+        round(final, 2)
         print("each person's cut is $", final)
     else:
         again()
     again()
 
 def again():
+    #validating user input for letter
     while True:
         try:
             q = input("do you want to calculate the tip again? (y or n)\n > ")
